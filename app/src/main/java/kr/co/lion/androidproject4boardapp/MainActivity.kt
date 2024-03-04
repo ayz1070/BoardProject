07867233f1283f00bd41ca019c59a9110102a86d
Jun <ayz1070@gmail.com>
2024-03-04 13:07:19 +0900

    feat: 앱 스플래쉬 화면 추가

--- app/src/main/java/kr/co/lion/androidproject4boardapp/MainActivity.kt
+++ app/src/main/java/kr/co/lion/androidproject4boardapp/MainActivity.kt
@@ -2,10 +2,17 @@ package kr.co.lion.androidproject4boardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kr.co.lion.androidproject4boardapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        installSplashScreen()
+
+        activityMainBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
